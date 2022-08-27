e519b64d1aa372cef41912566817968c1c97d81e
Camila Sury <camisury@gmail.com>
2022-08-27 01:26:07 -0300

    Histogramas

--- Examen Final.R
+++ Examen Final.R
@@ -39,6 +39,33 @@ municipiocaract<-municipio%>%left_join(caract_muni, by=c("cvemun"))
 
 ggplot(municipiocaract) + 
   geom_sf(colour='black', aes(fill=as.factor(urban),color=as.factor(urban)))
+  
+
+#Histograma informalidad 2000 
+hist(x = caract_muni$inf, prob = TRUE, main = "Informalidad mexicana por municipio en el 2000", 
+     xlab = "Informalidad en %", ylab = "Densidad",
+     col = "#AFEEEE", 
+     breaks = 20)
+grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
+hist(x = caract_muni$inf, prob = TRUE, main = "Informalidad mexicana por municipio en el 2000", 
+     xlab = "Informalidad en %", ylab = "Densidad",
+     add = TRUE, 
+     col = "#AFEEEE", 
+     breaks = 20)
+lines(density(caract_muni$inf), lwd = 2, col = '#6A5ACD')
+
+#Histograma insured 2000
+hist(x = caract_muni$insured, prob = TRUE, main = "Población mexicana asegurada por municipio en el 2000", 
+     xlab = "Asegurados en %", ylab = "Densidad",
+     col = "#FF7F50", 
+     breaks = 20)
+grid(nx = NA, ny = NULL, lty = 2, col = "gray", lwd = 1)
+hist(x = caract_muni$insured, prob = TRUE, main = "Poblacion mexicana asegurada por municipio en el2000", 
+     xlab = "Asegurados en %", ylab = "Densidad",
+     col = "#FF7F50", 
+     breaks = 20, 
+     add = TRUE)
+lines(density(caract_muni$insured), lwd = 2, col = '#8B1A1A')
 
 
 
